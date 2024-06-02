98a9ddc53336cb0ca222f91d2e787260ff48f3b4
russellandrady <russellsuper2000@gmail.com>
2024-06-02 19:52:50 +0530

    ui is completed

--- lib/utils/budget_tile.dart
+++ lib/utils/budget_tile.dart
@@ -1,3 +1,4 @@
+import 'package:budgetplanner/utils/popoup_box.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
@@ -5,6 +6,7 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 class BudgetTile extends StatelessWidget {
   String budgetName;
   int budgetAmount;
+
   Function(BuildContext)? onDelete;
   BudgetTile(
       {super.key,
@@ -14,53 +16,72 @@ class BudgetTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void createNewBudget() {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return PopoupBox(
+              budgetName: TextEditingController(text: budgetName),
+              budgetAmount:
+                  TextEditingController(text: budgetAmount.toString()),
+              onSave: () {},
+              loading: false,
+            );
+          });
+    }
+
     return Padding(
       padding: const EdgeInsets.only(top: 8, bottom: 8),
-      child: Slidable(
-        endActionPane: ActionPane(
-          motion: const StretchMotion(),
-          children: [
-            SlidableAction(
-              onPressed: onDelete,
-              icon: Icons.delete,
-              backgroundColor: Theme.of(context).primaryColorDark,
-              borderRadius: BorderRadius.circular(25),
-            )
-          ],
-        ),
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                width: 400,
-                height: 80,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColorLight,
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      budgetName,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
+      child: GestureDetector(
+        onLongPress: () {
+          createNewBudget();
+        },
+        child: Slidable(
+          endActionPane: ActionPane(
+            motion: const StretchMotion(),
+            children: [
+              SlidableAction(
+                onPressed: onDelete,
+                icon: Icons.delete,
+                backgroundColor: Theme.of(context).primaryColorDark,
+                borderRadius: BorderRadius.circular(25),
+              )
+            ],
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  width: 400,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorLight,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        budgetName,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "$budgetAmount\$",
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "$budgetAmount\$",
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
